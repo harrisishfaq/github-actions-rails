d5180dda6ad6213583d3b6b2db4570d62597973f
Harris Ishfaq <haris.ishfaq@icaremanager.com>
2025-02-04 16:56:38 +0500

    m

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,4 @@
-class BooksController = ApplicationController
+class BooksController << ApplicationController
   before_action :set_book, only: %i[ show edit update destroy ]
 
   # GET /books or /books.json
